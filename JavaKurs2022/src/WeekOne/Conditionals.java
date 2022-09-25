3e3454167b7b196716d40f95187d859bcd22c173
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-25 16:37:56 +0300

    if-else

---
+++ JavaKurs2022/src/WeekOne/Conditionals.java
@@ -0,0 +1,23 @@
+package WeekOne;
+
+public class Conditionals {
+
+	public static void main(String[] args) {
+	
+	     int sayi = 25;
+		
+		if (sayi<20 ) {
+			System.out.println("Sayı 20'den Küçüktür. ");
+			
+		} else if (sayi==20) {
+			System.out.println("Sayı 20'ye Eşittir.");
+			
+		}
+		else {
+			System.out.println("Sayı 20'den Küçük Değildir. ");
+			
+		}
+		 
+	}
+
+}
