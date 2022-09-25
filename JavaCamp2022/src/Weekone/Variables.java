9365ffb3301ce8af153167eec5bbc1e207f84b1c
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-25 17:15:18 +0300

    first add

---
+++ JavaCamp2022/src/Weekone/Variables.java
@@ -0,0 +1,16 @@
+package Weekone;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int ogrencisayisi = 12;
+		String mesaj = " Ögrenci sayısı :  ";
+		System.out.println(mesaj + ogrencisayisi);
+		System.out.println(mesaj + ogrencisayisi);
+
+		System.out.println("Ögrenci Sayısı : " + ogrencisayisi);
+		System.out.println("Ögrenci Sayısı : " + ogrencisayisi);
+		System.out.println("Ögrenci Sayısı : " + ogrencisayisi);
+	}
+
+}
