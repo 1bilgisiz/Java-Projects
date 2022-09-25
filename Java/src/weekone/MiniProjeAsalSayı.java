0777ef4507a06dc96b96e1770c6e67eb4e8cdd2b
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-25 23:49:55 +0300

    Asal sayı bulma

--- Java/src/weekone/MiniProjeAsalSayı.java
+++ Java/src/weekone/MiniProjeAsalSayı.java
@@ -5,34 +5,34 @@ import java.util.Iterator;
 public class MiniProjeAsalSayı {
 
 	public static void main(String[] args) {
-		int number =23;
-		
+		int number = 23;
+
 		int remainder = number % 2;
-		
+
 		boolean isPrime = true;
-		
-		if (number==1) {
+
+		if (number == 1) {
 			System.out.println("Sayı asal degildir.");
 			return;
-			
+
 		}
-		 if (number<1) {
+		if (number < 1) {
 			System.out.println("Geçersiz sayı !");
-		
+
 		}
-		 
-		 for (int i = 2; i < number; i++) {
-			if (number%i==0) {
+
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0) {
 				isPrime = false;
 			}
 		}
-		 
-		 if (isPrime) {
-			System.out.println(	"Sayı asaldır."	);
-		}else {
+
+		if (isPrime) {
+			System.out.println("Sayı asaldır.");
+		} else {
 			System.out.println("Sayı asal degildir.");
 		}
-		 
+
 	}
 
 }
