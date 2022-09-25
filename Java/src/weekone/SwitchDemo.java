99e4bb1d8294306870c0302761d17698ac890ef1
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-25 17:50:41 +0300

    switch-case

---
+++ Java/src/weekone/SwitchDemo.java
@@ -0,0 +1,33 @@
+package weekone;
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+		char grade = 'F';
+		
+		switch (grade) {
+		case 'A':
+			System.out.println("Mükemmel : Geçtiniz");
+			break;
+		case 'B':
+			System.out.println("Çok güzel : Geçtiniz");
+			break;
+		case 'C':
+			System.out.println("iyi : Geçtiniz");
+			break;
+		case 'D':
+			System.out.println("Orta: Geçtiniz");
+			break;
+		case 'F':
+			System.out.println("Malesef : Kaldınız ");
+			break;
+
+			
+			
+		default:
+			System.out.println("Geçersiz not girdiniz '");
+		}
+
+	}
+
+}
