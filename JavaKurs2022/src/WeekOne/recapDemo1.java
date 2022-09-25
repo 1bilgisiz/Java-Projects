a5af54f4db783acc2e6268ebb5e4922d91c4bc01
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-25 16:56:58 +0300

    recapDemo1

---
+++ JavaKurs2022/src/WeekOne/recapDemo1.java
@@ -0,0 +1,26 @@
+package WeekOne;
+
+public class recapDemo1 {
+
+	public static void main(String[] args) {
+		
+		int sayi1 = 25;
+		int sayi2 = 35;
+		int sayi3= 15;
+		int enBuyuk = sayi1;
+		
+		if (enBuyuk<sayi2) {
+			enBuyuk = sayi2;
+		}
+
+		if (enBuyuk<sayi3) {
+			enBuyuk = sayi3;
+			
+		}
+		
+		System.out.println( "En büyük sayı : " + enBuyuk );
+		
+		
+	}
+
+}
