fa03d9b4fa7048979a99ce2ab9825e6a6ae398cc
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-25 16:17:16 +0300

    Homework

---
+++ JavaKurs2022/src/WeekOne/Variables.java
@@ -0,0 +1,17 @@
+package WeekOne;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int  ogrencisayisi = 12;
+		String mesaj = " Ögrenci sayısı :  ";
+		System.out.println(mesaj + ogrencisayisi );
+		System.out.println(mesaj + ogrencisayisi );
+
+		System.out.println("Ögrenci Sayısı : " + ogrencisayisi );
+		System.out.println("Ögrenci Sayısı : " + ogrencisayisi );
+		System.out.println("Ögrenci Sayısı : " + ogrencisayisi );
+
+	}
+
+}
---
+++ JavaKurs2022/src/WeekOne/VeriTipleri.java
@@ -0,0 +1,22 @@
+package WeekOne;
+
+public class VeriTipleri {
+
+	public static void main(String[] args) {
+		
+		int sayi = 12;
+		
+		double sayi1 = 12.5;
+		
+		char karakter = 'A';
+		
+		boolean dogruMu = false;
+		
+		float sayi2= (float) 125.5 ;
+		
+		   
+		
+
+	}
+
+}
