96282f416c0a17c358bd61d1b7b99a412edf4e41
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-26 01:20:19 +0300

    miniproje3

---
+++ Java/src/weekone/MiniProje3.java
@@ -0,0 +1,26 @@
+package weekone;
+
+public class MiniProje3 {
+
+	public static void main(String[] args) {
+
+		//Mükemmel Sayı Bulma
+		
+		int sayi = 28 ;
+		int toplam =0;
+		
+		for (int i = 1; i < sayi; i++) {
+			if (sayi % i == 0) {
+				toplam = toplam+i;
+			}
+		}
+		
+		if (toplam==sayi) {
+			System.out.println("Mükemmel Sayı");
+		} else {
+			System.out.println("Mükemmel Sayı Degildir.");
+
+		}
+	}
+
+}
