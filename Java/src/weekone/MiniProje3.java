119de71d76922c00d7116b02dc3514d635c402ce
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-26 01:20:49 +0300

    miniproje3

--- Java/src/weekone/MiniProje3.java
+++ Java/src/weekone/MiniProje3.java
@@ -4,18 +4,18 @@ public class MiniProje3 {
 
 	public static void main(String[] args) {
 
-		//Mükemmel Sayı Bulma
-		
-		int sayi = 28 ;
-		int toplam =0;
-		
+		// Mükemmel Sayı Bulma
+
+		int sayi = 28;
+		int toplam = 0;
+
 		for (int i = 1; i < sayi; i++) {
 			if (sayi % i == 0) {
-				toplam = toplam+i;
+				toplam = toplam + i;
 			}
 		}
-		
-		if (toplam==sayi) {
+
+		if (toplam == sayi) {
 			System.out.println("Mükemmel Sayı");
 		} else {
 			System.out.println("Mükemmel Sayı Degildir.");
