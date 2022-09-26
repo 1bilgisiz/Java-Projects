ee5c9a4442cfafa56020825081961f5d27f89bc5
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-26 22:49:41 +0300

    variable args

---
+++ Java/src/weektwo/VariableArguments.java
@@ -0,0 +1,24 @@
+package weektwo;
+
+public class VariableArguments {
+
+	public static void main(String[] args) {
+
+		int toplam = toplama(2, 3, 6, 78, 9, 0);
+		System.out.println(toplam);
+
+	}
+
+	public static int toplama(int... sayilar) {
+		int toplam = 0;
+
+		for (int sayi : sayilar) {
+			toplam += sayi;
+
+		}
+
+		return toplam;
+
+	}
+
+}
