8f7a3895fdba6877817f3e53aa84675d1eda7b45
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-27 18:33:18 +0300

    recapdemo 1

---
+++ JavaKodlamaio-Week2/src/reCapDemo1/Main.java
@@ -0,0 +1,20 @@
+package reCapDemo1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		DortIslem dortIslem = new DortIslem();
+		int topla= dortIslem.Topla(3, 5);
+		int cikar=dortIslem.Cikar(2, 3);
+		int carp=dortIslem.Carp(1, 7);
+		int bol=dortIslem.Bol(9,3);
+		
+		System.out.println("Toplama sonucu : " + topla);
+		System.out.println("Çıkarma sonucu : " + cikar);
+		System.out.println("Çarpma sonucu : " + carp);
+		System.out.println("Bölme sonucu : " + bol);
+				
+	}
+
+}
