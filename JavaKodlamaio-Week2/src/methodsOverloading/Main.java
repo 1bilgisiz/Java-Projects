7be47b8f19694e6f25b1b41366b01807cc1c0efb
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-27 23:54:13 +0300

    Overloading

---
+++ JavaKodlamaio-Week2/src/methodsOverloading/Main.java
@@ -0,0 +1,13 @@
+package methodsOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(" Toplama Sonucu : " + dortIslem.topla(3, 5));
+		System.out.println(" Toplama Sonucu : " + dortIslem.topla(3, 5, 8));
+
+	}
+
+}
