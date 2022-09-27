6b59d539fdee0fa4fd638a62f835a552ce9eb15e
1bilgisiz <bilgicsaniye66@gmail.com>
2022-09-27 17:04:27 +0300

    classlarla çalışmak

---
+++ Java/src/classexample/Main.java
@@ -0,0 +1,14 @@
+package classexample;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.Add();
+		customerManager.Update();
+		customerManager.Remove();
+		customerManager.Sign();
+	}
+
+}
