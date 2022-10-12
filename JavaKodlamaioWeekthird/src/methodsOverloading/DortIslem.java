b0bc99493fe0e429229eb2e40f9a7c15093a195d
1bilgisiz <bilgicsaniye66@gmail.com>
2022-10-13 00:23:23 +0300

    overloading apply

---
+++ JavaKodlamaioWeekthird/src/methodsOverloading/DortIslem.java
@@ -0,0 +1,12 @@
+package methodsOverloading;
+
+public class DortIslem {
+	public int toplama(int a, int b) {
+		return a + b;
+	}
+
+	public int toplama(int a, int b, int c) {
+		return a + b + c;
+
+	}
+}
---
+++ JavaKodlamaioWeekthird/src/methodsOverloading/Main.java
@@ -0,0 +1,14 @@
+package methodsOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+
+		System.out.println(dortIslem.toplama(3, 5));
+
+		System.out.println(dortIslem.toplama(3, 6, 9));
+
+	}
+
+}
